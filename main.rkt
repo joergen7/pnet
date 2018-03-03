65493f97869fabf7f87ca6c7421234650664a0e0
Jörgen <joergen.brandt@onlinehome.de>
2018-03-03 20:52:01 +0100

    First impression.

--- main.rkt
+++ main.rkt
@@ -1,4 +1,64 @@
-#lang racket/base
+#lang racket
+
+(provide pnet-interface
+         start)
+
+(define pnet-interface
+  (interface () place-lst trsn-lst preset enabled? fire init-marking))
+
+(define (loop marking pnet)
+  ; TODO
+  (loop marking pnet))
+
+(define (start pnet)
+
+  (define marking0
+    (for/fold
+     ([marking (hash)])
+     ([place (send pnet place-lst)])
+     (hash-set marking place (send pnet init-marking place))))
+      
+  (define pnet-thread
+    (thread (lambda () (loop marking0 pnet))))
+  
+  (thread-send pnet-thread 'continue))
+
+
+(define cvm%
+  (class* object% (pnet-interface)
+
+    (define/public (place-lst)
+       '(coin-slot cash-box signal storage compartment))
+    
+    (define/public (trsn-lst)
+       '(a b))
+
+    (define/public (preset trsn)
+      (match trsn
+        ['a '(coin-slot)]
+        ['b '(signal storage)]))
+
+    (define/public (enabled? trsn marking)
+      #t)
+    
+    (define/public (fire trsn mode)
+      (match trsn
+        ['a (list 'produce (hash 'cash-box '(coin) 'signal '(sig)))]
+        ['b (list 'produce (hash 'compartment '(cookie-box)))]))
+    
+    (define/public (init-marking trsn)
+      (match trsn
+        ['storage '(cookie-box cookie-box cookie-box)]
+        [_        '()]))
+
+    (super-new)
+
+  ))
+
+
+
+
+
 
 (module+ test
   (require rackunit))
@@ -33,3 +93,7 @@
 (module+ main
   ;; Main entry point, executed when run with the `racket` executable or DrRacket.
   )
+
+
+
+
